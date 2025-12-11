a66f25a17f02a03e5dcd25c787ff45440ffd30e3
IgnacioGTri <896846@alu.murciaeduca.es>
2025-12-11 20:35:04 +0100

    segundo commit

--- Reto1.py
+++ Reto1.py
@@ -11,7 +11,8 @@ if os.path.exists(archivo):
         datos = [datos]
 else:
     datos = [] 
-numArticulos = 0
+
+
 for i in range (6):
     nombre = input("Introduce nombre del artículo: ")
     precio = float(input("Introduce precio: "))
@@ -37,4 +38,69 @@ with open(archivo, "r", encoding="utf-8") as jason:
 print("\nTu lista de artículos Navideños:")
 print(contenido)
 
-print("¿Quieres añadir algún regalo más? S/N")
+respuesta = "S"
+pregunta = "S"
+
+while pregunta == "S":
+    pregunta = input("¿Quieres añadir algún regalo más? S/N ").upper()
+
+    if pregunta == "S":
+
+       
+        if os.path.exists(archivo):
+            with open(archivo, "r", encoding="utf-8") as jason:
+                datos = json.load(jason)
+            if isinstance(datos, dict):
+                datos = [datos]
+        else:
+            datos = []
+
+        
+        nombre = input("Introduce nombre del artículo: ")
+        precio = float(input("Introduce precio: "))
+        cantidad = int(input("Introduce cantidad de artículos: "))
+
+        articuloNuevo = {
+            "nombre": nombre,
+            "precio": precio,
+            "cantidad": cantidad
+        }
+
+        datos.append(articuloNuevo)
+
+        with open(archivo, "w", encoding="utf-8") as jason:
+            json.dump(datos, jason, indent=4, ensure_ascii=False)
+
+        print("Artículo añadido correctamente.\n")
+        
+while respuesta == "S" :
+    respuesta= input("¿Quieres cambiar el precio de un artículo?(S/N)").upper()
+    
+    if respuesta != "S":
+        break
+    
+    if respuesta == "S":
+         nombre_buscar = input("Introduce el nombre del regalo a rectificar: ")
+
+    encontrado = False
+    for art in datos:
+        if art["nombre"].lower() == nombre_buscar.lower():
+            nuevo_precio = float(input("Introduce el nuevo precio: "))
+            art["precio"] = nuevo_precio
+            encontrado = True
+            break
+
+    if encontrado:
+        with open(archivo, "w", encoding="utf-8") as jason:
+            json.dump(datos, jason, indent=4, ensure_ascii=False)
+        print("Precio actualizado correctamente.")
+    else:
+        print("No se encontró un artículo con ese nombre.")
+        
+total = 0
+for art in datos:
+    total += art["precio"] * art["cantidad"]
+
+print(f"\nPrecio total de todos los regalos: {total:.2f} €")
+
+print("\n¡Adiós! ¡Felices Fiestas!")
